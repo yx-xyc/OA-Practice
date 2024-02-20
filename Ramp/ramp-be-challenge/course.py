40d13350c1a2800029e6a130a8382eb41e039dd5
yx-xyc <yx2021@nyu.edu>
2024-02-20 19:13:02 +0000

    add more ramp be code

--- Ramp/ramp-be-challenge/course.py
+++ Ramp/ramp-be-challenge/course.py
@@ -1,8 +1,16 @@
+from enum import Enum
+
+class GradingType(Enum):
+    NA = 0
+    STD = 1
+    PF = 2
+
 class Course:
-    def __init__(self, courseId: str, name: str, credits: int):
+    def __init__(self, courseId: str, name: str, credits: int, gradingType: GradingType = GradingType.NA):
         self.courseId = courseId
         self.name = name
         self.credits = credits
+        self.gradingType = gradingType
     def __str__(self):
         return f"Course ID: {self.courseId} \
             Name: {self.name} \
